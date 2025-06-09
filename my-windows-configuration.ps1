6cd0df4d564fd334b3949f17a5932deece4cde8e
Álysson Gleyson <alyssongleysondasilva@gmail.com>
2025-06-08 21:24:29 -0300

    feat: Checking the need to elevate permissions

---
+++ my-windows-configuration.ps1
@@ -0,0 +1,5 @@
+# Elevate permissions if necessary
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Warning "This script needs to be run as administrator."
+    exit 1
+}
