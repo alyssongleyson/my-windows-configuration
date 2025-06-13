3e29dafa59e753e1c7ddfce990eae4a93aca26f7
Álysson Gleyson <alyssongleysondasilva@gmail.com>
2025-06-13 19:59:11 -0300

    feat: Add Chocolatey installation process

--- my-windows-configuration.ps1
+++ my-windows-configuration.ps1
@@ -3,3 +3,11 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
     Write-Warning "This script needs to be run as administrator."
     exit 1
 }
+
+# Install Chocolatey (if not already installed)
+if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+    Write-Host "Installing Chocolatey..."
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
